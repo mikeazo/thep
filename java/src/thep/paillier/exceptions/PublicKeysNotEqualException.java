93a2eff4dc72e34101b344e567b24e042dc8ecf4
undefinedspace <undefinedspace@62a23742-27b5-11df-ab0a-9f3f633ae91d>
2010-03-10 21:50:16 +0000

    added a message to the exception constructor

--- java/src/thep/paillier/exceptions/PublicKeysNotEqualException.java
+++ java/src/thep/paillier/exceptions/PublicKeysNotEqualException.java
@@ -6,5 +6,9 @@ public class PublicKeysNotEqualException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public PublicKeysNotEqualException(String msg) {
+		super(msg);
+	}
 
 }
