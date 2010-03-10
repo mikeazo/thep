3078de7b736aac74b871c3d05f1af0beb09aabc7
undefinedspace <undefinedspace@62a23742-27b5-11df-ab0a-9f3f633ae91d>
2010-03-10 21:50:35 +0000

    initial commit

---
+++ java/src/thep/paillier/exceptions/VectorSizesNotEqualException.java
@@ -0,0 +1,13 @@
+package thep.paillier.exceptions;
+
+public class VectorSizesNotEqualException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public VectorSizesNotEqualException(String msg) {
+		super(msg);
+	}
+
+}
